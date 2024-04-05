e774e8982c1253b0d216c708bfd5c49bf1c943ab
keeva <keevang.design@gmail.com>
2024-04-05 11:52:20 -0400

    edit

--- js/weather.js
+++ js/weather.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 const apiKey = process.env.API_KEY;
+// const apiKey = "07d67f84cd34e2452408a52a5a0c1414";
 const baseURL = "https://api.openweathermap.org/data/2.5/weather?";
 
-// const apiKey = "07d67f84cd34e2452408a52a5a0c1414";
 // const cityName = "Toronto";
 // const countryCode = "CA";
 
