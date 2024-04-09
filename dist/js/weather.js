515d33deb22ad8855a7262c7f92d8d815b3359ff
keeva <keevang.design@gmail.com>
2024-04-09 13:40:03 -0400

    added dist folder

---
+++ dist/js/weather.js
@@ -0,0 +1,40 @@
+require("dotenv").config();
+const axios = require("axios");
+
+const apiKey = process.env.API_KEY;
+const baseURL = "https://api.openweathermap.org/data/2.5/weather?";
+
+function getWeatherData(cityName, countryCode, resultElementId) {
+  const url = `${baseURL}q=${cityName},${countryCode}&appid=${apiKey}&units=metric`;
+
+  $.ajax({
+    url: url,
+    type: "GET",
+    success: function (data) {
+      displayWeatherData(data, resultElementId);
+    },
+  });
+}
+
+function displayWeatherData(data, resultElementId) {
+  const timezoneOffsetInSeconds = data.timezone;
+  const localDate = new Date((data.dt + timezoneOffsetInSeconds) * 1000);
+  var iconcode = data.weather[0].icon;
+  var iconurl = "https://openweathermap.org/img/wn/" + iconcode + "@2x.png";
+
+  const displayData = `
+        <h4>${localDate.toLocaleString()}</h4>
+        <h1>${data.name}, ${data.sys.country}</h1>
+        <img src='${iconurl}'>
+        <h3> ${data.main.temp} °C</h3>
+        <p>Weather: ${data.weather[0].main}</p>
+        <p>Humidity: ${data.main.humidity}%</p>
+    `;
+
+  $(`#${resultElementId}`).html(displayData);
+}
+
+$(document).ready(function () {
+  getWeatherData("Toronto", "CA", "weatherResult");
+  getWeatherData("Hong Kong", "HK", "weatherHKResult");
+});
